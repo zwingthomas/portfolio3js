50cf5be708819103d079fb8ab67cd3752d76ced4
zwingthomas <zwingthomas@gmail.com>
2025-03-28 17:02:58 -0500

    Postcss and autoprefixer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,21 @@ import { styles } from '../styles';
 import { navLinks } from '../constraints';
 import { logo, menu, close } from '../assets';
 
+const [active, setActive] = useState('');
+
 const Navbar = () => {
   return (
-    <nav className={`${styles.paddingX}`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/" className="flex items-center gap-2" onClick={() => {
+          setActive("");
+          window.scrollTo(0, 0);
+        }}> 
+          {/* TODO: visit logo.com to generate a new logo */}
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">Zwingers <span>| Premium Engineers </span></p>
+        </Link>
+      </div>
     </nav>
   )
 }
