ff69ed8654dd578c49946e75b2d688243522415f
zwingthomas <zwingthomas@gmail.com>
2025-03-31 13:29:39 -0500

    feat: navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,10 +22,7 @@ const Navbar = () => {
         >
           {/* TODO: visit logo.com to generate a new logo */}
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Zwingers{' '}
-            <span className="hidden sm:text-black">| Premium Engineers</span>
-          </p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">Zwingers&nbsp;<span className="hidden sm:block"> | Premium Engineers</span></p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((Link) => (
