97700cbc1b8c7934c1799eca894ff9df742644c3
zwingthomas <zwingthomas@gmail.com>
2025-03-31 13:04:50 -0500

    build: use tailwindcss v3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { styles } from '../styles';
 import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
-
-
 
 const Navbar = () => {
   const [active, setActive] = useState('');
@@ -13,23 +11,37 @@ const Navbar = () => {
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link to="/" className="flex items-center gap-2" onClick={() => {
-          setActive('');
-          window.scrollTo(0, 0);
-        }}> 
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive('');
+            window.scrollTo(0, 0);
+          }}
+        >
           {/* TODO: visit logo.com to generate a new logo */}
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">
-            Zwingers 
-            <span className="sm:black hidden">
-              | Premium Engineers 
-            </span>
+            Zwingers{' '}
+            <span className="hidden sm:text-black">| Premium Engineers</span>
           </p>
         </Link>
-        <p className="text-red-500"></p>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? 'text-white' : 'text-secondary'
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
