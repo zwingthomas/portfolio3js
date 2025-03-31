06c8b5f672bb3301822f6c34d7dc4857681971e3
zwingthomas <zwingthomas@gmail.com>
2025-03-31 12:36:45 -0500

    bug: site now runs on npm run dev

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,19 @@ import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import { styles } from '../styles';
-import { navLinks } from '../constraints';
+import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
-const [active, setActive] = useState('');
+
 
 const Navbar = () => {
+  const [active, setActive] = useState('');
+
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link to="/" className="flex items-center gap-2" onClick={() => {
-          setActive("");
+          setActive('');
           window.scrollTo(0, 0);
         }}> 
           {/* TODO: visit logo.com to generate a new logo */}
