aa1832f6a0d48c41e12b4e923deb6e3fcfb38ab7
zwingthomas <zwingthomas@gmail.com>
2025-04-01 18:50:33 -0500

    feat: model loading in

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,9 +8,23 @@ const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black"/>
-      <pintLight intensity={1}/>
-      <primitive object={computer.scene}/>
+      <hemisphereLight intensity={1.5} groundColor="black"/>
+      <pointLight intensity={1}/>
+      {/* TODO: Spotlight not working */}
+      <spotLight 
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive 
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   )
 }
@@ -26,14 +40,15 @@ const ComputersCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls 
           enableZoom={false}
-          maxPolarAndle={Math.PI / 2}
-          minPolarAngle={Math.Pi / 2}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
           />
         <Computers />
-      </Suspense>
+      </ Suspense>
+
       <Preload all />
     </Canvas>
   )
 }
 
-export default Computers
+export default ComputersCanvas;
