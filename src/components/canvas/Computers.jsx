d46baaddcd9c44916bc1f286095f2d92e4f0fb3e
Thomas Zwinger <zwingthomas@gmail.com>
2025-04-07 14:35:32 -0500

    patch: mobile positioning and sizing for computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,12 +4,12 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ( {isMobile} ) => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
     <mesh>
-      <hemisphereLight intensity={1.5} groundColor="black"/>
-      <pointLight intensity={1}/>
+      <hemisphereLight intensity={isMobile ? 3 : 1.5} groundColor="black"/>
+      <pointLight intensity={isMobile ? 2 : 1}/>
       {/* TODO: Spotlight not working */}
       <spotLight 
         position={[-20, 50, 10]}
@@ -21,8 +21,8 @@ const Computers = () => {
       />
       <primitive 
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.2 : 0.75}
+        position={isMobile ? [0, 0, -0.35] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -30,6 +30,30 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+
+  useEffect(() => {
+    
+    // listen for changes to screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    
+    // set initial value
+    setIsMobile(mediaQuery.matches);
+
+    // callback function definition
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+    // add callback as listener for changes
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    // remove on unmount
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, [])
+
   return (
     <Canvas
       frameLoop="demand"
@@ -43,7 +67,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
           />
-        <Computers />
+        <Computers isMobile={isMobile}/>
       </ Suspense>
 
       <Preload all />
