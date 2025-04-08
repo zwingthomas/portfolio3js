acd9fdbc6fe7bc7ead5261ccb3d17d5b13599612
Thomas Zwinger <zwingthomas@gmail.com>
2025-04-08 15:52:32 -0500

    feat: basic movable tech balls

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,8 +6,8 @@ const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className="w-2 h-28" key={technology.name}>
-          <BallCanvas icon={technologies.icon}/>
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon}/>
         </div>
       ))}
     </div>
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -11,8 +11,24 @@ const Ball = (props) => {
     <Float speed={1.75} rotationIntensity={1}>
       <ambientLight intensity={0.25}/>
       <directionalLight position={[0, 0, 0.05]}/>
-      <mesh castShadow receiveShadow scale={2.75}>
+      <mesh 
+        castShadow 
+        receiveShadow 
+        scale={2.75}
+      >
         <icosahedronGeometry args={[1,1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0,0,1]}
+          map={decal}
+          rotation={[ 2 * Math.PI, 0, 6.25]}
+          flatShading
+        />
       </mesh>
     </Float>
   )
