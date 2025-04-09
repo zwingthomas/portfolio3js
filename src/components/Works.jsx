acc554e69ca25f7338de8f4452bd81bc4f7f37f9
LeticeZwinger <leticedss@gmail.com>
2025-04-09 15:19:00 -0500

    Update Works.jsx
    create My Work content
    
    Co-Authored-By: Thomas Zwinger <zwingthomas@gmail.com>

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,5 @@
 import Tilt from "react-tilt";
+import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { github } from "../assets";
@@ -7,7 +8,27 @@ import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
-  return <></>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My works</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          {" "}
+          Following projects showcases my skills and experiences through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories and live demos in it. It reflects my
+          ability to solve complex problems, work with different technologies,
+          and manage projects effectively.
+        </motion.p>
+      </div>
+    </>
+  );
 };
 
 export default SectionWrapper(Works, "");
