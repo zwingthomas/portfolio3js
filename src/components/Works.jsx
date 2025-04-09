24fe32447e5bd4cd01ee7a417174701b81562910
LeticeZwinger <leticedss@gmail.com>
2025-04-09 15:01:17 -0500

    Update Works.jsx
    add all the imports needed
    
    Co-Authored-By: Thomas Zwinger <zwingthomas@gmail.com>

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
+  return <></>;
+};
 
-export default Works
+export default SectionWrapper(Works, "");
