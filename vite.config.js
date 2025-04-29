9540ca3771135177e58accf9e04281ce6e35eb08
Thomas Zwinger <zwingthomas@gmail.com>
2025-04-28 21:44:14 -0500

    deploy: simple deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio3js/",
   resolve: {
     alias: {
       'framer-motion': path.resolve(__dirname, 'node_modules/framer-motion/dist/cjs/index.js'),
